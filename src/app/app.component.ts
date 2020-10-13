446b8f825019bd0c7aadddb7f13679229ea8efde
Hem <hem@privatecircle.co>
2020-10-13 23:12:14 +0530

    api related tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,35 @@
 import { LoganService } from './logan.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title: any;
+  apiData: any;
 
-  constructor ( private router: Router, private loganServ: LoganService ) {
+  selectedName: any;
 
-  }
+  constructor(private router: Router, private loganServ: LoganService) {}
 
   showName() {
     this.loganServ.showMsg('Logan in Service');
     // this.title = 'LOGAN'
     this.router.navigate(['/home']);
   }
+
+  ngOnInit() {
+    this.loganServ.getApiData().subscribe( res => {
+      this.apiData = res;
+      console.log("API\n", this.apiData)
+    })
+  }
+
+  getName(data) {
+    this.selectedName = data;
+  }
+
 }
--- src/app/logan.service.ts
+++ src/app/logan.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,9 +8,14 @@ import { Injectable } from '@angular/core';
 
 export class LoganService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   showMsg( hh) {
     console.log(hh);
+  }
+
+  getApiData() {
+    let endpoint = `https://jsonplaceholder.typicode.com/users`;
+    return this.http.get(endpoint);
   }
 }
